8e5d0d3f69dfa354e7d3dc29f762923d05730fb3
Shawn Nectar <shawnmctc@gmail.com>
2023-04-15 19:02:41 -0300

    remaking 2.0 - delta_t, jd and jde

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # import math
-import time
-import pytz
 import threading
-
+import time
 # import numpy as np
 from datetime import datetime
+
+import pytz
 # import matplotlib.pyplot as plt
 from timezonefinder import TimezoneFinder
+
 
 class SunPosition:
     def __init__(self):
@@ -25,13 +26,14 @@ class SunPosition:
         self.year = 0
         self.month = 0
         self.day = 0
+        self.hour = 0
         self.minute = 0
         self.second = 0
-        self.day_decimal = 0
+        self.day_decimal = None
         self.delta_t = 0
         self.utc_time = 0
 
-# Dependencies Date Get
+    # Dependencies Date Get
     def get_timezone_and_utc(self):
         tf = TimezoneFinder()
         while True:
@@ -39,6 +41,8 @@ class SunPosition:
             # Location
             latitude = self.latitude
             longitude = self.longitude
+
+            utc_time = self.utc_time
 
             # Getting Timezone Area
             self.latitude = -23.326388680858557
@@ -53,17 +57,62 @@ class SunPosition:
                 # Exporting
                 self.utc_time = utc_time
 
-                print(utc_time)
+            if utc_time != 0:
+                self.year = utc_time.year
+                self.month = utc_time.month
+                self.day = utc_time.day
+                self.hour = utc_time.hour
+                self.minute = utc_time.minute
+                self.second = utc_time.second
 
             time.sleep(1)
 
+    # Delta T and Julian Day and Julian Ephemeris Day
+    def julian_day_and_ephemeris_day(self):
+        while True:
+            # Importing Dependencies
+            year = self.year
+            month = self.month
+            day = self.day
+            hour = self.hour
+            minute = self.minute
+            second = self.second
+            day_decimal = self.day_decimal
+            delta_t = self.delta_t
+            utc_time = self.utc_time
+            jd = self.jd
+            jde = self.jde
 
-# Julian Day and Julian Ephemeris Day
+            # Define day decimal:
+            day_decimal = hour + minute / 60 + second / 3600
+
+
+            if year and month and day_decimal != 0:
+                # Calculating delta_t
+                t = (year - 1820) / 100
+                delta_t = 62.92 + 0.32217 * t + 0.005589 * t ** 2
+
+                # Calculating Julian Day
+                jd = int(365.25 * (year + 4716)) + int(30.6001 * (month + 1)) + day_decimal - 1524.5
+
+                # Calculating Julian Ephemeris Day
+                jde = jd + (delta_t / 86400)
+
+                self.delta_t = delta_t
+                self.jd = jd
+                self.jde = jde
+
+            time.sleep(1)
+
 
 # Julian Century and Julian Ephemeris Century and Julian Ephemeris Millennium
 
 
 # MultiThreading
 sun_position = SunPosition()
+
 get_timezone_thread = threading.Thread(target=sun_position.get_timezone_and_utc)
 get_timezone_thread.start()
+
+julian_day_and_ephemeris_day_thread = threading.Thread(target=sun_position.julian_day_and_ephemeris_day)
+julian_day_and_ephemeris_day_thread.start()
